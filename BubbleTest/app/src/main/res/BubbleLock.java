a1c8621a83a910ce128873190bb17a106f17015c
Alex Ballet <alexandre.ballet@keylightech.com>
2017-05-15 14:06:43 +0200

    bubbleLock class

---
+++ BubbleTest/app/src/main/res/BubbleLock.java
@@ -0,0 +1,17 @@
+import android.graphics.Color;
+
+/**
+ * Created by Alxd on 15/05/2017.
+ */
+
+public class BubbleLock {
+
+    private Integer diameter;
+
+    private Color color;
+
+    private String description;
+
+    private Lock lock;
+
+}
